3980453e71f519d16f62e6d07a6d8c4059bccac1
Proud-Sachinda <30469646+Proud-Sachinda@users.noreply.github.com>
2019-04-06 00:24:52 +0200

    Upload Project
    Upload the initial project files for getting started

---
+++ PTT InClinic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PTT_InClinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
